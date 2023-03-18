dfa0c976ad718ef6864fed00f496a29b75a398ec
Roman <roman@macbook.local>
2023-03-18 12:58:15 +0200

    feature/1 Added header

---
+++ src/components/Header/Header.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="Header">
+     <div class="header">
+        <div class="wrapper-header">
+
+           <nav class="navbar navbar-light bg-light">
+             <a class="navbar-brand justify-content-start" href="#">DataCose</a>
+             <div class="buttons m-auto pr-lg-5 pr-md-5">
+               <button class="border btn btn-primary p-2">Go to Posts page</button>
+               <button class="border btn btn-primary p-2">Go to Todos page</button>
+             </div>
+           </nav>
+
+        </div>
+     </div>
+  </div>
+</template>
+
+<script>
+
+ export default {
+   name: "Header",
+   data() {
+     return {
+
+     }
+   }
+ }
+
+</script>
+
+<style scoped>
+
+</style>
