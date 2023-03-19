0dde8ebe95d46f64841cd72c7da62645c4ca8b2f
Roman <roman@macbook.local>
2023-03-19 18:21:47 +0200

    feature/3 Added Posts page

--- src/pages/Posts.vue
+++ src/pages/Posts.vue
@@ -1,21 +1,94 @@
 <template>
   <div id="Posts">
-    <span>This page POSTS</span>
+     <div class="posts">
+        <div class="wrapper-posts">
+
+           <div class="search-block text-center mb-5 mt-4">
+             <p>You can find record in the table to colum "Title"</p>
+             <input class="search" v-model="input_text" />
+           </div>
+
+<!--           <p>{{ pullUser() }}</p>-->
+
+           <div class="table-block">
+              <div class="container">
+                 <div class="row justify-content-center">
+                    <div class="col">
+
+                       <table class="table">
+                          <tr>
+                             <th>Title</th>
+                             <th>Body</th>
+                          </tr>
+                          <tr v-for="item in pullInfo" :key="item.id" v-on:click="nameUser( item.userId )">
+                             <td>{{ item.title }}</td>
+                             <td>{{ item.body }}</td>
+                          </tr>
+                       </table>
+                    </div>
+                 </div>
+              </div>
+           </div>
+
+        </div>
+     </div>
   </div>
 </template>
 
 <script>
- export default {
-   name: "Posts",
-   data() {
-     return {
+import { mapGetters, mapActions, mapMutations} from "vuex";
 
-     }
-   }
+ export default {
+    name: "Posts",
+    data() {
+       return {
+         input_text: "",
+         user: ""
+       }
+    },
+
+    methods: {
+      ...mapActions(["getInfo", "getInfoUsers"]),
+      ...mapMutations(["filterTable"]),
+
+       nameUser( value ) {
+
+          this.getInfoUsers( value );
+
+       },
+
+    },
+
+    watch: {
+
+       input_text(value) {
+          this.filterTable( value );
+       }
+
+    },
+
+    computed: {
+       ...mapGetters(["pullInfo", "pullUser"]),
+
+    },
+
+    async mounted() {
+
+      this.getInfo();
+      this.getInfoUsers();
+
+
+
+    },
+
+
  }
 
 </script>
 
 <style scoped>
-
+ .table th,
+ .table td {
+  border: 1px solid #333333;
+ }
 </style>
